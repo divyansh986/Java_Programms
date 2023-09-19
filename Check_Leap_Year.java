a052d74064689d08e8ed60438fb9ad8660e40d2a
Divyansh Gupta <91428398+divyansh986@users.noreply.github.com>
2023-09-19 22:04:49 +0530

    Check_Leap_Year

---
+++ Check_Leap_Year.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        
+        //if century year then divisible by 400.
+        //if non century year then divisible by 4.
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+        scanner.close();
+    }
+}
