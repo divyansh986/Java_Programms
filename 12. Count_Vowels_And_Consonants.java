3841328831f0a60d221c552e514b440f935915db
Divyansh Gupta <91428398+divyansh986@users.noreply.github.com>
2023-09-21 22:24:50 +0530

    Count_Vowels_And_Consonants

---
+++ 12. Count_Vowels_And_Consonants.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CountVowelsAndConsonants {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine().toLowerCase();
+        
+        int vowels = 0, consonants = 0;
+        for (char ch : input.toCharArray()) {
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+        
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        
+        scanner.close();
+    }
+}
